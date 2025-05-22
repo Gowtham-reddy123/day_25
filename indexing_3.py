34631e100aedff3baba1547f0b12dcbafb8ae230
Gowtham-reddy123 <reddyg026@gmail.com>
2025-05-22 13:46:56 +0530

    tasks

---
+++ indexing_3.py
@@ -0,0 +1,13 @@
+a="LAYOUT"
+print(a)
+b=a[3]+a[4]+a[5]
+print(b)
+c=a[0]+a[1]+a[2]
+print(c)
+d=a[-1]+a[-2]+a[-3]+a[-4]+a[-5]+a[-6]
+print(d)
+e=a[2]+a[3]+a[4]
+print(e)
+f=a[-4]+a[-3]+a[-2]
+print(f)
+
---
+++ indexing_6.py
@@ -0,0 +1,9 @@
+a="DRAWING CANVAS"
+b=a[-6]+a[-5]+a[-4]
+print(b)
+c=a[11]+a[9]+a[10]
+print(c)
+d=a[1]+a[2]+a[3]
+print(d)
+e=a[4]+a[5]+a[6]
+print(e)
